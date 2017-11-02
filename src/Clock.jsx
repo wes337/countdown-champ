a1e74ac037cb9e258ce099275301da4e0726d49c
wes337 <wesmoses@gmail.com>
2017-11-02 21:26:19 +0200

    hooking with lifecycle methods

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -12,19 +12,24 @@ class Clock extends Component {
         }
     }
 
+    componentWillMount() {
+        this.getTimeUntil(this.props.deadline);
+    }
+
+    componentDidMount() {
+        setInterval(() => this.getTimeUntil(this.props.deadline), 1000);
+    }
+
     getTimeUntil(deadline) {
         const time = Date.parse(deadline) - Date.parse(new Date());
-        console.log('time', time);
         const seconds = Math.floor((time/1000) % 60);
         const minutes = Math.floor((time/1000/60) % 60);
         const hours = Math.floor((time/(1000*60*60)) % 24);
         const days = Math.floor(time/(1000*60*60*24));
-        console.log('days', days, 'minutes', minutes, 'hours', hours, 'seconds', seconds);
+        this.setState({days, hours, minutes, seconds});
     }
 
     render() {
-        this.getTimeUntil(this.props.deadline);
-
         return (
             <div>
                 <div className="Clock-days">{this.state.days} days</div>
