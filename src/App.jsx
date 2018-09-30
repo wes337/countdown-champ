d4e789362fce1cd7afda061c2cf6cf09dbbbeea2
Wes Moses <wesmoses@gmail.com>
2018-09-30 11:22:57 +0300

    updated for 2018

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            deadline: 'December 25, 2017',
+            deadline: 'December 25, 2018',
             newDeadline: ''
         }
     }
